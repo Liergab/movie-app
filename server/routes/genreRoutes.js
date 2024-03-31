86e0f4f624c4c66b1984c78d504ca3d046efd4e5
Bryan Gabriel <bryangabrielberja25@gmail.com>
2024-03-31 18:33:27 +0800

    AUTHMID_AND_ADMINAUTH_SET_UP

--- server/routes/genreRoutes.js
+++ server/routes/genreRoutes.js
@@ -1,10 +1,11 @@
 import express         from 'express'
 import * as controller from '../controller/genreController.js'
+import { AdminMiddleware, AuthMiddleware } from '../middleware/AuthMiddlware.js'
 const genreRouter = express.Router()
 
-genreRouter.post('/', controller.createGenre)
-genreRouter.put('/:id', controller.updateGenre)
-genreRouter.delete('/:id', controller.deleteGenre)
-genreRouter.get('/', controller.getAllGenre)
-genreRouter.get('/:id/individual', controller.getGenreById)
+genreRouter.post('/',  [AuthMiddleware, AdminMiddleware], controller.createGenre)
+genreRouter.put('/:id',[AuthMiddleware, AdminMiddleware], controller.updateGenre)
+genreRouter.delete('/:id',[AuthMiddleware,AdminMiddleware], controller.deleteGenre)
+genreRouter.get('/',   [AuthMiddleware,AdminMiddleware],    controller.getAllGenre)
+genreRouter.get('/:id/individual', [AuthMiddleware,AdminMiddleware], controller.getGenreById)
 export default genreRouter
