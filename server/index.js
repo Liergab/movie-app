78cf13160f2601454b86af977d46eb2fa6d7ec04
Bryan Gabriel <bryangabrielberja25@gmail.com>
2024-04-07 16:59:04 +0800

    cors

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import express       from 'express';
 import cookieParser  from 'cookie-parser';
 import rootRouter    from './routes/index.js';
 import db            from './config/db.js';
-import cors          from 'cors'
+// import cors          from 'cors'
 import {PageNotFound, error} from './middleware/ErrorMiddleware.js';
 
 const app = express()
@@ -11,10 +11,10 @@ const PORT = process.env.PORT
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({
-    credentials: true,
-    origin: 'https://brygab-movie-app.vercel.app/', // Replace with your frontend's URL
-}))
+// app.use(cors({
+//     credentials: true,
+//     origin: 'https://brygab-movie-app.vercel.app/', // Replace with your frontend's URL
+// }))
 app.use(express.static('public'))
 app.use(express.urlencoded({extended:true}))
 
