ab82c87513f33c4a735904b92438818c93ced011
Bryan Gabriel <bryangabrielberja25@gmail.com>
2024-04-12 10:08:27 +0800

    Expiration

--- server/util/GenerateToken.js
+++ server/util/GenerateToken.js
@@ -2,14 +2,14 @@ import jwt from 'jsonwebtoken'
 
 const GenerateToken = async(res, userId) => {
     const token = jwt.sign({userId}, process.env.JWT_SECRET_KEY,{
-        expiresIn:'7s',
+        expiresIn:'30d',
     })
 
   res.cookie('jwt',token,{
     httpOnly: true,
     secure: process.env.NODE_ENV !== 'development', // Use secure cookies in production
     sameSite: 'None', // Prevent CSRF attacks
-    maxAge: 30 * 1000, // 30 day
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 day
   })
 
    
