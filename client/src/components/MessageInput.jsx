685b94dc7ad9d602aaf35cc2ede51b4521eac8c9
Bryan Gabriel <bryangabrielberja25@gmail.com>
2024-04-18 23:04:14 +0800

    Message textbox

--- client/src/components/MessageInput.jsx
+++ client/src/components/MessageInput.jsx
@@ -5,15 +5,23 @@ import React from 'react'
 const MessageInput = ({handleClick,onChange, value}) => {
   return (
     
-    <div className='flex items-center  justify-center sticky w-full bottom-0 bg-slate-950 p-4  gap-2'>
+    <div className='flex items-center  justify-center sticky w-full bottom-0 bg-slate-800 p-4  gap-2 border-t-2  border-t-slate-900'>
         <input 
             type='text' 
             placeholder='Message' 
             value={value}
-            className='w-full max-w-96 py-2 border border-slate-800 px-2 rounded-sm' 
+            className='w-full max-w-96 py-2 border-4 border-white px-2 rounded-lg' 
             onChange={onChange}
         />
-        <Button variant='outlined'  endIcon={<IoSend />} size='large' onClick={handleClick}>
+        <Button 
+          variant='outlined'  
+          size='large' onClick={handleClick}
+          endIcon={<IoSend />}  
+          sx={{
+            border:'2px solid #0f172a',
+            
+          }}
+        >
             send
         </Button>
     </div>
